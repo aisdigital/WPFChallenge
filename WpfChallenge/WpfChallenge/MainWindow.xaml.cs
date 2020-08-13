2201f1776b36f4c1764e7c638f17ec9b4dd06c62
Rafael <akafael@users.noreply.github.com>
2020-08-13 13:25:49 -0300

    Add basic structure

--- WpfChallenge/WpfChallenge/MainWindow.xaml.cs
+++ WpfChallenge/WpfChallenge/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfChallenge.ViewModel;
 
 namespace WpfChallenge
 {
@@ -23,6 +24,8 @@ namespace WpfChallenge
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new CustomerViewModel();
         }
     }
 }
---
+++ WpfChallenge/WpfChallenge/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfChallenge.Models;
+
+namespace WpfChallenge.ViewModel
+{
+    class CustomerViewModel :INotifyPropertyChanged
+    {
+        public Customer selectedCustomer { get; set; }
+        public ObservableCollection<Customer> customers { get; set; }
+        public CommandBinding NewCommand;
+
+        public CustomerViewModel()
+        {
+            selectedCustomer = new Customer();
+            customers = new ObservableCollection<Customer>
+            {
+                new Customer{name="John Due",phone="",address=""}
+            };
+
+        }
+
+
+        private ICommand UpdateCommander;
+
+
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (UpdateCommander == null)
+                    UpdateCommander = new Updater();
+                return UpdateCommander;
+            }
+            set
+            {
+                UpdateCommander = value;
+            }
+        }
+
+        private class Updater : ICommand
+        {
+            #region ICommand Members  
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+
+            }
+
+            #endregion
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+
+}
