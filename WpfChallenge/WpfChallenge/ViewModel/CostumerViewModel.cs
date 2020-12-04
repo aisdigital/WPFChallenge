87376a1efa1c615c950c5a20841627301d851f10
Lineu Júnior <analista.lineu@gmail.com>
2020-12-04 12:24:29 -0300

    [ New ] - WpfChallenge - Lineu Júnior

---
+++ WpfChallenge/WpfChallenge/ViewModel/CostumerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using WpfChallenge.Model;
+using WpfChallenge.Controller;
+
+namespace WpfChallenge.ViewModel
+{
+    public class CostumerViewModel : ObservableCollection<Costumer>
+    {
+        CostumerController costumer = new CostumerController();
+        public CostumerViewModel()
+        {
+            readyCostumerCollection();
+        }
+
+        private void readyCostumerCollection()
+        {
+            foreach (var item in costumer.getAllCostumer())
+            {
+                Add(item);
+            }                       
+        }
+    }
+}
