05f0354f7e071814ce44abea18f00ae0d3b85636
Guilherme Dantas <guiiabdn@gmail.com>
2021-01-28 16:36:45 -0300

    Alteração na Configuração da Conexão com o banco.

--- WpfChallenge/WpfChallenge.Repository/Data/Context.cs
+++ WpfChallenge/WpfChallenge.Repository/Data/Context.cs
@@ -24,8 +24,7 @@ namespace WpfChallenge.Repository.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                //optionsBuilder.UseSqlServer("Server=prod-sql.prosegnet.com.br;Database=MultCalcSeg;User Id=proseg;Password=b123;");
+                optionsBuilder.UseSqlServer("Server=prod-sql.prosegnet.com.br;Database=Ilia;User Id=userIlia;Password=Ilia@#$;");
             }
         }
 
