8c89f031b2ea21ec85267741b806a708641bd8ce
vagnerrmello <vagnerrmello@gmail.com>
2021-02-01 12:47:41 -0300

    teste vagnerrodrigues

---
+++ WpfChallenge/WpfChallenge/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace WpfChallenge.Models
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+    }
+}
